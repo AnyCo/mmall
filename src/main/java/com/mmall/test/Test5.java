ab3909fc5e2883003e055a1f3084f3cf98cc7529
AnyCo <768840497@qq.com>
2018-04-26 14:31:09 +0800

    '4-26v1'

--- src/main/java/com/mmall/test/Test5.java
+++
@@ -1,4 +0,0 @@
-package com.mmall.test;
-
-public class Test5 {
-}
--- src/main/java/com/mmall/test/Test7.java
+++
@@ -1,9 +0,0 @@
-package com.mmall.test;
-
-public class Test7 {
-
-    public static void main(String[] args) {
-        System.out.println("test7");
-        System.out.println("last modify");
-    }
-}
